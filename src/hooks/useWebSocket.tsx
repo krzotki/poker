ac24acd9f953a0842b48c0b3735ebc69b31ff193
Karol Rzotki <karol.rzotki@brainly.com>
2022-05-16 21:36:26 +0200

    debug

--- src/hooks/useWebSocket.tsx
+++ src/hooks/useWebSocket.tsx
@@ -8,6 +8,8 @@ export const useWebSocket = () => {
     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
     const address = `${protocol}://${window.location.hostname}:8080`;
     const ws = new WebSocket(address);
+    console.log({protocol, address, ws});
+    ws.onopen = () => console.log('OPEN')
     setWebSocket(ws);
     return () => {};
   }, []);
